35f7ed6483e7800dd6800116531c2ca4eda83f2b
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-10-17 12:12:35 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,92 +1,90 @@
 #include "main.h"
 
-void clean_up_va_list(va_list args, custom_buffer_t *output);
-int process_printf(const char *format, va_list args, custom_buffer_t *output);
-int custom_printf(const char *format, ...);
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
- * clean_up_va_list - Performs cleanup operations for custom_printf.
- * @args: A va_list of arguments provided to custom_printf.
- * @output: A custom_buffer_t struct.
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
  */
-void clean_up_va_list(va_list args, custom_buffer_t *output)
+void cleanup(va_list args, buffer_t *output)
 {
-    va_end(args);
-    write(1, output->start, output->len);
-    free_custom_buffer(output);
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
 /**
- * process_printf - Reads through the format string for custom_printf.
+ * run_printf - Reads through the format string for _printf.
  * @format: Character string to print - may contain directives.
- * @output: A custom_buffer_t struct containing a buffer.
+ * @output: A buffer_t struct containing a buffer.
  * @args: A va_list of arguments.
  *
  * Return: The number of characters stored to output.
  */
-int process_printf(const char *format, va_list args, custom_buffer_t *output)
+int run_printf(const char *format, va_list args, buffer_t *output)
 {
-    int i, width, precision, result = 0;
-    char temp;
-    unsigned char flags, length;
-    unsigned int (*specifier_function)(va_list, custom_buffer_t *,
-                                       unsigned char, int, int, unsigned char);
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
-    for (i = 0; *(format + i); i++)
-    {
-        length = 0;
-        if (*(format + i) == '%')
-        {
-            temp = 0;
-            flags = handle_custom_flags(format + i + 1, &temp);
-            width = handle_custom_width(args, format + i + temp + 1, &temp);
-            precision = handle_custom_precision(args, format + i + temp + 1,
-                                                &temp);
-            length = handle_custom_length(format + i + temp + 1, &temp);
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
 
-            specifier_function = handle_custom_specifiers(format + i + temp + 1);
-            if (specifier_function != NULL)
-            {
-                i += temp + 1;
-                result += specifier_function(args, output, flags, width, precision, length);
-                continue;
-            }
-            else if (*(format + i + temp + 1) == '\0')
-            {
-                result = -1;
-                break;
-            }
-        }
-        result += _custom_memcpy(output, (format + i), 1);
-        i += (length != 0) ? 1 : 0;
-    }
-    clean_up_va_list(args, output);
-    return (result);
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
 }
 
 /**
- * custom_printf - Outputs a formatted string.
+ * _printf - Outputs a formatted string.
  * @format: Character string to print - may contain directives.
  *
  * Return: The number of characters printed.
  */
-int custom_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-    custom_buffer_t *output;
-    va_list args;
-    int result;
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
-    output = init_custom_buffer();
-    if (output == NULL)
-        return (-1);
+	va_start(args, format);
 
-    va_start(args, format);
+	ret = run_printf(format, args, output);
 
-    result = process_printf(format, args, output);
-
-    return (result);
+	return (ret);
 }
-
